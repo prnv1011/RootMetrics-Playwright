826fa4d0eff1d3ac6ac3db40ce6fd88733795291
prnv1011 <80803284+prnv1011@users.noreply.github.com>
2026-09-02 23:31:26 +0530

    test(rootinsights): add oauth login and notification dismissal tests

--- tests/test_validate_login_logout_functionality.py
+++ tests/test_validate_login_logout_functionality.py
@@ -34,3 +34,17 @@ def test_login_logout_flow_with_overall_and_text(valid_login):
     overall_summary_page.click_user_menu()
     overall_summary_page.click_logout()
     overall_summary_page.verify_login_page_title()
+
+
+from pages.overall_summary_page import OverallSummaryPage
+
+
+def test_selected_chart_highlight(valid_login):
+    overall_summary_page = OverallSummaryPage(valid_login)
+    overall_summary_page.verify_summary_page_title()
+    overall_summary_page.handle_welcome_popup()
+    overall_summary_page.click_and_verify_overall()
+    overall_summary_page.click_and_verify_text()
+    overall_summary_page.click_user_menu()
+    overall_summary_page.click_logout()
+    overall_summary_page.verify_login_page_title()
