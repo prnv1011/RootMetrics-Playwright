71d590d290197f12ed08471f1afbe15d66ff1148
prnv1011 <80803284+prnv1011@users.noreply.github.com>
2026-09-02 20:43:03 +0530

    test(auth): add login logout flow coverage

--- tests/test_validate_login_logout_functionality.py
+++ tests/test_validate_login_logout_functionality.py
@@ -9,3 +9,14 @@ def test_login_logout_flow(valid_login):
     overall_summary_page.click_logout()
     overall_summary_page.verify_login_page_title()
 
+
+from pages.overall_summary_page import OverallSummaryPage
+
+
+def test_login_logout_flow(valid_login):
+    overall_summary_page = OverallSummaryPage(valid_login)
+    overall_summary_page.verify_summary_page_title()
+    overall_summary_page.handle_welcome_popup()
+    overall_summary_page.click_user_menu()
+    overall_summary_page.click_logout()
+    overall_summary_page.verify_login_page_title()
